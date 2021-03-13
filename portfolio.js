abe2f5471061b269b53413810ddefde4b3525296
sophia-w-he <sh787@cornell.edu>
2021-03-12 20:08:36 -0800

    adding project images mobile

---
+++ portfolio.js
@@ -0,0 +1,11 @@
+"use strict";
+(function() {
+
+
+  window.onload = function() {
+    if(/iP(hone|ad)/.test(window.navigator.userAgent)) {
+        document.body.addEventListener('touchstart', function() {}, false);
+    }
+}
+
+})();
